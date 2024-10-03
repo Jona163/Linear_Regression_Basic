1c817432b5c7fd9659cc5005b403b085885473d1
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-02 21:16:17 -0600

    Update Linear Regresion.py

--- Linear Regresion.py
+++ Linear Regresion.py
@@ -9,3 +9,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
